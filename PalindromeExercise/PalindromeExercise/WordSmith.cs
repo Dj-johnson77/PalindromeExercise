6bdcc93eda343027ccf6224f8e06208bb77013ea
Donald Johnson <donaldrjohnson66@gmail.com>
2023-01-10 19:34:10 -0600

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith() { }
+        public bool IsAPalindrome(string a)
+        {
+            string backwards = "";
+            if (a != null && a != "")
+            {
+                for (int i = a.Length - 1; i >= 0; i--)
+                {
+                    backwards += a[i];
+                }
+                if (a == backwards)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+            
+            
+           
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
+using PalindromeExercise;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("sad", false)]
+        [InlineData("run", false)]
+        [InlineData("noon", true)]
+        [InlineData("wow", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string a, bool expected)
         {
+            //Arrange
+            var w = new WordSmith();
 
+            //Act
+            var actual = w.IsAPalindrome(a);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
